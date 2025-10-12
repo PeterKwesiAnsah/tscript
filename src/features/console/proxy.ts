c4aa781aecdf24ab08750a0e2dc414dcc6e2b352
PeterKwesiAnsah <ansahpeter123@yahoo.com>
2025-10-12 13:24:29 +0000

    proxy console namespace

---
+++ src/features/console/proxy.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type ConsoleMessage = {
+	method: keyof Console;
+	args: any[];
+	timestamp: number;
+};
+
+export class ConsoleProxy {
+	private originalConsole: Partial<Console> = {};
+	private listeners: ((msg: ConsoleMessage) => void)[] = [];
+
+	constructor() {
+		this.patch();
+	}
+
+	private patch() {
+		const methods: (keyof Console)[] = [
+			"log",
+			"info",
+			"warn",
+			"error",
+			"debug",
+			"table",
+			"dir",
+		];
+
+		for (const method of methods) {
+			// @ts-expect-error Keep the original reference
+			this.originalConsole[method] = console[method];
+			//@ts-expect-error missing function signature
+			console[method] = (...args: any[]) => {
+				// Notify listeners
+				const message: ConsoleMessage = {
+					method,
+					args,
+					timestamp: Date.now(),
+				};
+				this.listeners.forEach((fn) => fn(message));
+			};
+		}
+	}
+
+	onMessage(fn: (msg: ConsoleMessage) => void) {
+		this.listeners.push(fn);
+	}
+
+	restore() {
+		for (const [method, original] of Object.entries(this.originalConsole)) {
+			// @ts-expect-error restoring type
+			console[method] = original;
+		}
+	}
+}
