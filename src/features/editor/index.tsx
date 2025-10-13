413068c6a54d529905af1b6e60ef4574b51e52e1
PeterKwesiAnsah <ansahpeter123@yahoo.com>
2025-10-13 20:49:26 +0000

    run model content code

--- src/features/editor/index.tsx
+++ src/features/editor/index.tsx
@@ -1,27 +1,20 @@
-import React from 'react';
-import * as monaco from 'monaco-editor';
-import { useGetActiveModel } from './store/models';
-import defaultTheme from '@/monaco/themes/default.json';
-import { resolveDTFiles } from '../resolver/declarationfiles';
-
-import path from 'path-browserify';
-import {
-	// parseSourceToDep,
-	// parseSourceToDepPath,
-	parseSourceToOriginalDep,
-} from '../resolver/core/parseSource';
-//import path from 'path-browserify';
+import React from "react";
+import * as monaco from "monaco-editor";
+import { useGetActiveModel } from "./store/models";
+import defaultTheme from "@/monaco/themes/default.json";
+import runCode from "../runtime/runCode";
 
 let timeoutId: NodeJS.Timeout;
 const EditorInstance = () => {
 	const activeModel = useGetActiveModel();
 
 	React.useEffect(() => {
+		//Typescript Compiler Options
 		monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
 			allowNonTsExtensions: true,
 			//This need to be set always for type intellisense with third lib parties to work
 			moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
-			noEmit: true,
+			noEmit: false,
 		});
 	}, []);
 	function subscribeToModelChanges(model: monaco.editor.ITextModel) {
@@ -31,32 +24,44 @@ const EditorInstance = () => {
 			if (!modelContent.length) {
 				return;
 			}
-			const rootDir = path.dirname(model.uri.toString());
-			const extraLibs =
-				monaco.languages.typescript.typescriptDefaults.getExtraLibs();
-			console.log(parseSourceToOriginalDep(modelContent));
-
-			for (const dep of parseSourceToOriginalDep(modelContent)) {
-				// if (dep.type !== 'package') continue;
-				//we can link other ts modules with this??
-				const libUri =
-					rootDir + `/node_modules/${dep.importResource}/index.d.ts`;
-				const findExtraLib = extraLibs[libUri];
-				if (findExtraLib) continue;
-
-				//TODO: cache dts files (offline)
-				const packageResolvedDTS = await resolveDTFiles(dep.importStatement);
-				console.log(packageResolvedDTS.includes('renderToReadableStream'));
-
-				monaco.languages.typescript.typescriptDefaults.addExtraLib(
-					packageResolvedDTS,
-					libUri
-				);
-				/**
-				 * TODO:using monaco.languages.typescript.typescriptDefaults.getExtraLibs() an extra lib has already been added
-				 * prevening uneccesary api calls
-				 * */
+			if (!model.uri.path.endsWith(".ts")) {
+				runCode(modelContent);
+				return;
 			}
+
+			const worker = await monaco.languages.typescript.getTypeScriptWorker();
+			const client = await worker(model.uri);
+			const result = await client.getEmitOutput(model.uri.toString());
+			runCode(result.outputFiles[0].text);
+
+			// const rootDir = path.dirname(model.uri.toString());
+			// const extraLibs =
+			// 	monaco.languages.typescript.typescriptDefaults.getExtraLibs();
+			// console.log(parseSourceToOriginalDep(modelContent));
+
+			//This piece of code handles .d.ts files resolution but for now my focus is getting the javascript running
+			//and then later use web containers to handle this
+			// for (const dep of parseSourceToOriginalDep(modelContent)) {
+			// 	// if (dep.type !== 'package') continue;
+			// 	//we can link other ts modules with this??
+			// 	const libUri =
+			// 		rootDir + `/node_modules/${dep.importResource}/index.d.ts`;
+			// 	const findExtraLib = extraLibs[libUri];
+			// 	if (findExtraLib) continue;
+
+			// 	//TODO: cache dts files (offline)
+			// 	const packageResolvedDTS = await resolveDTFiles(dep.importStatement);
+			// 	console.log(packageResolvedDTS.includes("renderToReadableStream"));
+
+			// 	monaco.languages.typescript.typescriptDefaults.addExtraLib(
+			// 		packageResolvedDTS,
+			// 		libUri
+			//);
+			/**
+			 * TODO:using monaco.languages.typescript.typescriptDefaults.getExtraLibs() an extra lib has already been added
+			 * prevening uneccesary api calls
+			 * */
+			//}
 		}, 2000);
 	}
 
@@ -71,13 +76,13 @@ const EditorInstance = () => {
 				fileURI
 			);
 			//@ts-expect-error Type 'string' is not assignable to type 'BuiltinTheme'.
-			monaco.editor.defineTheme('shadcnTheme', defaultTheme);
+			monaco.editor.defineTheme("vs-dark", defaultTheme);
 
 			const sharedEditorOptions = {
 				minimap: { enabled: false },
 				automaticLayout: true,
 				scrollBeyondLastLine: false,
-				theme: 'shadcnTheme',
+				theme: "vs-dark",
 			};
 			const editor = monaco.editor.create(node, {
 				model,
@@ -87,7 +92,7 @@ const EditorInstance = () => {
 		},
 		[activeModel]
 	);
-	return <div ref={createMonacoEditor} className="h-screen"></div>;
+	return <div ref={createMonacoEditor} className="h-screen w-[70%]"></div>;
 };
 
 export default EditorInstance;
