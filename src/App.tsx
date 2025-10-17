3fcebbe7e5aa8eead2361e8ae264625ed640a4a4
PeterKwesiAnsah <ansahpeter123@yahoo.com>
2025-10-17 09:55:48 +0000

    add console window

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import EditorInstance from "./features/editor";
 import Toolbar from "./features/toolbar";
+import Console from "./features/console";
 
 function App() {
-  return (
-    <section className="flex flex-col">
-      <Toolbar />
-      <main>
-        <EditorInstance />
-      </main>
-    </section>
-  );
+	return (
+		<section className="flex flex-col">
+			<Toolbar />
+			<main className="flex flex-row">
+				<EditorInstance />
+				<div id="console_container" className="w-0 transition-all duration-300 ease-in-out overflow-hidden">
+					<Console />
+				</div>
+			</main>
+		</section>
+	);
 }
 
 export default App;
